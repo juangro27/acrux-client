d9c7db29b38a3b43a7017535016dfb69595392a3
John Doe <juangnba@hotmail.com>
2023-04-19 18:31:57 +0200

    changes in navigation

--- src/components/Navigation/Navigations.jsx
+++ src/components/Navigation/Navigations.jsx
@@ -103,19 +103,39 @@ const Navigation = () => {
 
                     <Disclosure.Panel className="sm:hidden">
                         <div className="space-y-1 px-2 pb-3 pt-2">
-                            {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        "text-teal-300 hover:bg-teal-700 hover:text-white",
-                                        "block rounded-md px-3 py-2 text-base font-medium"
-                                    )}
-                                >
-                                    {item.name}
-                                </Disclosure.Button>
-                            ))}
+                            {!user
+                                ? navigation.map((item) => (
+                                      <Disclosure.Button
+                                          key={item.name}
+                                          as="a"
+                                          href={item.href}
+                                          className={classNames(
+                                              "text-teal-300 hover:bg-teal-700 hover:text-white",
+                                              "block rounded-md px-3 py-2 text-base font-medium"
+                                          )}
+                                      >
+                                          {item.name}
+                                      </Disclosure.Button>
+                                  ))
+                                : navigationLogged.map((item) => (
+                                      <Link
+                                          to={item.href}
+                                          onClick={
+                                              item.name === "Logout" && logout
+                                          }
+                                      >
+                                          <Disclosure.Button
+                                              key={item.name}
+                                              as="p"
+                                              className={classNames(
+                                                  "text-teal-300 hover:bg-teal-700 hover:text-white",
+                                                  "block rounded-md px-3 py-2 text-base font-medium"
+                                              )}
+                                          >
+                                              {item.name}
+                                          </Disclosure.Button>
+                                      </Link>
+                                  ))}
                         </div>
                     </Disclosure.Panel>
                 </>
